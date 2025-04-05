5dd0fab81e9c9c7c8185574b97a919c9aae01375
Erisson Cavalcanti <133155578+ErissonCavalcanti@users.noreply.github.com>
2025-04-06 00:02:49 +0100

    eventos de menu

--- code/Menu.py
+++ code/Menu.py
@@ -4,7 +4,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, C_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, C_WHITE, COLOR_YELLOW
 
 
 class Menu:
@@ -15,6 +15,8 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        global i
+        menu_option = 0
         pygame.mixer_music.load("./asset/Menu.mp3")
         pygame.mixer_music.play(-1)
 
@@ -24,14 +26,29 @@ class Menu:
             self.menu_text(50, text="Futurista", text_color=(COLOR_ORANGE), text_center_pos=((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], C_WHITE, text_center_pos=((WIN_WIDTH / 2), 200 + 30 * i))
-
-            pygame.display.flip()
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_YELLOW, text_center_pos=((WIN_WIDTH / 2), 200 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, text_center_pos=((WIN_WIDTH / 2), 200 + 30 * i))
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close window
                     quit()  # End game
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+
+            pygame.display.flip()
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
 
@@ -39,18 +56,3 @@ class Menu:
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.window.blit(source=text_surf, dest=text_rect)
-
-        if text_color == C_WHITE:
-            padding = 10  # espaço extra ao redor do texto
-            bg_rect = Rect(
-                text_rect.left - padding,
-                text_rect.top - padding,
-                text_rect.width + 2 * padding,
-                text_rect.height + 2 * padding
-            )
-            roxo = (128, 0, 128)  # cor roxa (RGB)
-            pygame.draw.rect(self.window, roxo, bg_rect, border_radius=5)
-
-        self.window.blit(source=text_surf, dest=text_rect)
-        
-
